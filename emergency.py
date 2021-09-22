ec948dd3d69c6a6fcfad606c2d407b41f28463be
Data-Science-SWASFT <86771505+Data-Science-SWASFT@users.noreply.github.com>
2021-09-22 14:06:29 +0100

    Update emergency.py

--- emergency.py
+++ emergency.py
@@ -44,7 +44,6 @@ if uploaded_file is not None:
     
     else: 
         st.warning("Please name your target date column ds or dst within your uploaded data to continue")
-        st.stop()
              
     
 target = st.sidebar.selectbox('Choose column you would like to forecast',df.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64']).columns.tolist(), help = 'The programme will automatically find columns that can be forecasted, just select from this list when you have imported a dataset')
