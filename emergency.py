e3381a3ff68c573773cc3ce87fb2a2b5684607f3
Data-Science-SWASFT <86771505+Data-Science-SWASFT@users.noreply.github.com>
2021-09-22 13:56:26 +0100

    Update emergency.py

--- emergency.py
+++ emergency.py
@@ -46,7 +46,7 @@ if uploaded_file is not None:
     elif 'dst' in df:
         df['dst'] = pd.to_datetime(df['dst'], format='%d/%m/%Y %H:%M')
         df = df.set_index('dst')
-        df.rename(columns={"dst": "ds")
+        df.rename(columns={"dst": "ds"})
     
 target = st.sidebar.selectbox('Choose column you would like to forecast',df.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64']).columns.tolist(), help = 'The programme will automatically find columns that can be forecasted, just select from this list when you have imported a dataset')
 st.sidebar.text(''' ''')
