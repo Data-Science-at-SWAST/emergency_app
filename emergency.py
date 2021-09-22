028605f3d58df4af26282679bae0f45e3d1a230f
Data-Science-SWASFT <86771505+Data-Science-SWASFT@users.noreply.github.com>
2021-09-22 13:45:15 +0100

    Update emergency.py

--- emergency.py
+++ emergency.py
@@ -32,11 +32,20 @@ if uploaded_file is not None:
     except:
         df = pd.read_excel(uploaded_file)
     
-    if 'ds' not in df:
+    if 'ds' not in df or 'dst' not in df:
         st.warning("Please name your target date column ds within your uploaded data to continue")
         st.stop()
-    df['ds'] = pd.to_datetime(df['ds'], format='%d/%m/%Y')
-    df = df.set_index('ds')
+        
+        
+        
+    elif 'ds' in df:
+       
+        df['ds'] = pd.to_datetime(df['ds'], format='%d/%m/%Y')
+        df = df.set_index('ds')
+        
+    elif 'dst' in df:
+        df['dst'] = pd.to_datetime(df['dst'], format='%d/%m/%Y %H:%M')
+        df = df.set_index('dst')
     
 target = st.sidebar.selectbox('Choose column you would like to forecast',df.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64']).columns.tolist(), help = 'The programme will automatically find columns that can be forecasted, just select from this list when you have imported a dataset')
 st.sidebar.text(''' ''')
